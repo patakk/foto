00a27e931088ebe29f58f2b97551c294046f1108
patakk <paolo.ceric@gmail.com>
2022-04-27 00:17:39 +0200

    2022

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from flask import render_template
 import base64
 import requests
+from flask import Flask, render_template, request, \
+                  Response, send_file, redirect, url_for, make_response, jsonify
 
 app = Flask(__name__)
+application = app
 
 
 @app.route("/")
--- static/script.js
+++ static/script.js
@@ -1,7 +1,33 @@
 
 
 function preventBehavior(e) {
-e.preventDefault(); 
+    e.preventDefault(); 
 };
-document.addEventListener("touchmove", preventBehavior, {passive: false}); 
+
+
+function saveOnMouseDown(){
+    el = document.getElementById( "save-label");
+    el.style.background = "#FFFFFF";
+    el.style.color = "#101010";
+}
+
+function saveOnMouseUp(){
+    var el = document.getElementById("save-label");
+    el.style.background = "#101010";
+    el.style.color = "#FFFFFF"; 
+
+    requestSave()
+}
+
+
+$(document).ready(function(){
+    document.addEventListener("touchmove", preventBehavior, {passive: false}); 
  
+    document.getElementById('save-label').onmouseup = saveOnMouseUp;
+    document.getElementById('save-label').onmousedown = saveOnMouseDown;
+
+    //var path = document.getElementById('img').src;
+    //var ext = path.slice(path.length-3, path.length);
+    //if (ext == "png" || ext == "jng")
+    //    document.getElementById('img').style.visibility = "visible";
+});
