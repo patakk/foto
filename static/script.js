2569ef4e67e36815dcdedcbcbdd29c2e112c7f57
patakk <paolo.ceric@gmail.com>
2022-04-27 00:53:50 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -60,15 +60,22 @@ $(document).ready(function(){
     document.getElementById('save-label').onmousedown = saveOnMouseDown;
 
 
-    let camera_button = document.querySelector("#start-camera");
     let video = document.querySelector("#video");
     let click_button = document.querySelector("#click-photo");
     let canvas = document.querySelector("#canvas");
+    
+    const constraints = {
+        audio: false,
+        video:
+        {
+          facingMode: { exact: "environment" }
+        }
+      }
 
-    camera_button.addEventListener('click', async function() {
-        let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        video.srcObject = stream;
-    });
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            player.srcObject = stream;
+        });
 
     click_button.addEventListener('click', function() {
         canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
