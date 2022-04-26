d501eeed830491c3e00137c245c6d633a467cb0f
patakk <paolo.ceric@gmail.com>
2022-04-27 00:47:53 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -65,10 +65,8 @@ $(document).ready(function(){
     let click_button = document.querySelector("#click-photo");
     let canvas = document.querySelector("#canvas");
 
-    camera_button.addEventListener('click', async function() {
-        let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        video.srcObject = stream;
-    });
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
 
     click_button.addEventListener('click', function() {
         canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
