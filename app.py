1555eadd914b562a1a33d38a0a108755dbc083be
patakk <paolo.ceric@gmail.com>
2022-04-27 23:21:07 +0200

    2022

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ application = app
 @app.route('/save_info', methods=['GET', 'POST'])
 def save_info():
     if request.method == 'POST':
-        #image_b64 = request.values['image']
+        image_b64 = request.values['image']
         #imgstr = re.search(r'data:image/png;base64,(.*)',image_b64).group(1)
         #decoded = base64.b64decode(imgstr)
         #with open('./static/output.png', 'wb') as f:
         #    f.write(decoded)
-        return jsonify({'name': './static/output.png', 'w': 111, 'h': 222})
+        return jsonify({'name': './static/output.pngaaaaa', 'w': 111, 'h': 222})
 
 
 @app.route("/")
