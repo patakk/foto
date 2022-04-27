fe9956cee5104a0953bc598187b65f40edbcf68a
patakk <paolo.ceric@gmail.com>
2022-04-27 03:35:52 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -78,8 +78,8 @@ $(document).ready(function(){
     document.getElementById('save-label').addEventListener('touchend', handleEnd);
 
     Webcam.set({
-        width: window.innerWidth*16/9*0.7,
-        height: window.innerWidth*0.7,
+        width: 720,
+        height: 1280,
         image_format: 'jpeg',
         jpeg_quality: 90
     });
