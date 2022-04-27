54b58b371325f31d0cd7a58f0b074606e17a366d
patakk <paolo.ceric@gmail.com>
2022-04-27 03:49:11 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -85,9 +85,6 @@ $(document).ready(function(){
     });
     Webcam.attach( '#player' );
 
-    document.querySelector('video').style.left = window.innerWidth*0.7*.5;
-    document.querySelector('video').style.top = 0;
-
     //var path = document.getElementById('img').src;
     //var ext = path.slice(path.length-3, path.length);
     //if (ext == "png" || ext == "jng")
