7b24a444d98f96138913abf8f0dbfab978eb1dc5
patakk <paolo.ceric@gmail.com>
2022-04-27 02:21:39 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -65,7 +65,7 @@ $(document).ready(function(){
     document.getElementById('save-label').onmousedown = saveOnMouseDown;
 
 
-    let video = document.querySelector("#player");
+    let videoel = document.querySelector("#player");
     let click_button = document.querySelector("#click-photo");
     let canvas = document.querySelector("#canvas");
     
@@ -84,15 +84,14 @@ $(document).ready(function(){
             },
             facingMode: "environment"
         }
-    }
+    };
 
-    const videoStream = await navigator.mediaDevices.getUserMedia(constraints)
-    video.srcObject = videoStream
-    video.setAttribute('playsinline', true);
+    const videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoel.srcObject = videoStream
 
     click_button.addEventListener('click', function() {
         
-        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+        canvas.getContext('2d').drawImage(videoel, 0, 0, canvas.width, canvas.height);
         let image_data_url = canvas.toDataURL('image/jpeg');
 
         // data url of the image
