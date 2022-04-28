d96a4842ed2bb57d261cc062005ab1a0eb2b9fe5
patakk <paolo.ceric@gmail.com>
2022-04-28 04:52:27 +0200

    2022

--- app.py
+++ app.py
@@ -75,20 +75,11 @@ def session():
     
 @app.route("/preview", methods=['GET', 'POST'])
 def preview():
-    if request.method == 'POST':
-        userName = request.values['userName']
-        userName = 'Ggt'
-        userDir = './static/database/{}'.format(userName)
-        if not os.path.exists(userDir):
-            return render_template('preview.html')
-        rolls = os.listdir(userDir)
-        return render_template('previewRolls.html', rollslen=len(rolls), rolls=rolls)
-
     return render_template('preview.html')
     
-    
+
 @app.route("/previewRolls", methods=['GET', 'POST'])
-def preview():
+def previewRolls():
     if request.method == 'GET':
         userName = request.values['userName']
         userDir = './static/database/{}'.format(userName)
