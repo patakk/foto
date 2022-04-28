479b75f7b0b578e7d7195ed37683cc5b50555ba0
patakk <paolo.ceric@gmail.com>
2022-04-28 03:33:41 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -191,7 +191,7 @@ function animationLoop(){
         saveLabel.style.top = formRect['bottom']*1. + innerH*0.02 + "px";
         saveLabel.style.left = window.innerWidth*0.5 - saveRect["width"]/2;
         document.getElementById("back-button").style.top = formRect['bottom']*1. + innerH*0.02 + "px";
-        document.getElementById("back-button").style.left = "20px";
+        document.getElementById("back-button").style.left = "10px";
     }
     catch(err) {
         document.getElementById("message").innerHTML = err.message;
