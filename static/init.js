67f2b7ed8107a2e4cdaa90914a28a21504c7fb7c
patakk <paolo.ceric@gmail.com>
2022-04-28 03:00:43 +0200

    2022

---
+++ static/init.js
@@ -0,0 +1,22 @@
+
+        
+$(document).ready(function(){
+    document.addEventListener("touchmove", preventBehavior, {passive: false}); 
+    
+    
+    previewLabel = document.getElementById("session-button");
+    sessionLabel = document.getElementById("preview-button");
+
+    var sessionRect = sessionLabel.getBoundingClientRect();
+    var previewRect = sessionLabel.getBoundingClientRect();
+
+    sessionLabel.style.left = window.innerWidth/2 - sessionRect['width']/2 + "px";
+    sessionLabel.style.top = window.innerHeight/2 - sessionRect['height'] + "px";
+    previewLabel.style.left = window.innerWidth/2 - previewRect['width']/2 + "px";
+    previewLabel.style.top = window.innerHeight/2 + "px";
+
+    //var path = document.getElementById('img').src;
+    //var ext = path.slice(path.length-3, path.length);
+    //if (ext == "png" || ext == "jng")
+    //    document.getElementById('img').style.visibility = "visible";
+});
