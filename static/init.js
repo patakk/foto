9327e88ba432d0c1789868dcb788879b898a1329
patakk <paolo.ceric@gmail.com>
2022-04-28 03:03:41 +0200

    2022

--- static/init.js
+++ static/init.js
@@ -3,17 +3,14 @@
 $(document).ready(function(){
     document.addEventListener("touchmove", preventBehavior, {passive: false}); 
     
-    
     previewLabel = document.getElementById("session-button");
     sessionLabel = document.getElementById("preview-button");
 
     var sessionRect = sessionLabel.getBoundingClientRect();
-    var previewRect = sessionLabel.getBoundingClientRect();
+    var previewRect = previewLabel.getBoundingClientRect();
 
-    sessionLabel.style.position = "absolute";
     sessionLabel.style.left = window.innerWidth/2 - sessionRect['width']/2 + "px";
     sessionLabel.style.top = window.innerHeight/2 - sessionRect['height'] + "px";
-    previewLabel.style.position = "absolute";
     previewLabel.style.left = window.innerWidth/2 - previewRect['width']/2 + "px";
     previewLabel.style.top = window.innerHeight/2 + "px";
 
