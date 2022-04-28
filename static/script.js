40940a4162fe2280c6a3f93043a30c2b32728674
patakk <paolo.ceric@gmail.com>
2022-04-28 02:05:10 +0200

    2022

--- static/script.js
+++ static/script.js
@@ -45,7 +45,7 @@ function requestSave(data_uri){
             success: function(resp) {
 
                 if(resp["message"]){
-                    saveLabel.style.background = "#20FF80";
+                    saveLabel.style.background = "#109930";
                     saveLabel.style.color = "#FFFFFF";
                     saveLabel.innerHTML = "&nbsp;&nbsp;SAVED!&nbsp;&nbsp;";
                     timer = 1300;
