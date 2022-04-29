e40cbcd3bb6095bf039bfcd6c7bdf758323bdfdd
patakk <paolo.ceric@gmail.com>
2022-04-30 01:22:07 +0200

    2022

--- static/js/roll.js
+++ static/js/roll.js
@@ -2,3 +2,20 @@ function goBack() {
     userName = window.location.search.split("&")[0].split("=")[1];
     window.location.href = '/foto/preview?userName=' + userName;
 }
+
+
+$(document).ready(function(){
+    document.addEventListener("touchmove", preventBehavior, {passive: false}); 
+        
+
+    window.requestAnimationFrame(animationLoop);
+
+    if(window.innerWidth > window.innerHeight){
+        document.getElementById('rootElement').style.width = "800px";
+    }
+
+    //var path = document.getElementById('img').src;
+    //var ext = path.slice(path.length-3, path.length);
+    //if (ext == "png" || ext == "jng")
+    //    document.getElementById('img').style.visibility = "visible";
+});
