b4842c52d8db41ee2d04e9ee0b97e94ec803618d
patakk <paolo.ceric@gmail.com>
2022-04-29 05:56:26 +0200

    2022

--- app.py
+++ app.py
@@ -90,8 +90,7 @@ def previewRolls():
             frames = glob.glob(os.path.join(rollDir, '/*.json'))
             frames = [json.load(open(frame)) for frame in frames]
             print(frames)
-            print(safas)
-            return render_template('roll.html', nframes=len(frames), frames=frames)
+            return render_template('roll.html', nframess=len(frames), frames=frames)
         else:
             userName = request.values['userName']
             userDir = './static/database/{}'.format(userName)
