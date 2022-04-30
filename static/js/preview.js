4b880a7f789e43c54781ede16053250f281543d6
patakk <paolo.ceric@gmail.com>
2022-04-30 22:23:42 +0200

    2022

--- static/js/preview.js
+++ static/js/preview.js
@@ -32,7 +32,7 @@ function animationLoop(){
     //form.style.top = window.innerHeight*.5 + "px";
     //form.style.width = width*.85 + "px";
     var formRect = form.getBoundingClientRect();
-    //nameLabel.style.width = width*.85 - 8 + "px";
+    nameLabel.style.width = "100%";
 
     //submitLabel.style.position = "absolute";
     //submitLabel.style.left = width*.075 + "px";
