7e25510ebade1450777b998893071690d9357614
patakk <paolo.ceric@gmail.com>
2022-04-30 03:46:45 +0200

    2022

--- static/js/session.js
+++ static/js/session.js
@@ -216,7 +216,7 @@ function animationLoop(){
 }
 
 $(document).ready(function(){
-    document.addEventListener("touchmove", preventBehavior, {passive: false}); 
+    //document.addEventListener("touchmove", preventBehavior, {passive: false}); 
     
     form = document.getElementById("form");
     snapLabel = document.getElementById("snap-button");
