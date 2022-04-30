1ff9cc9648b8432af15446c456e97765f81ad12c
patakk <paolo.ceric@gmail.com>
2022-04-30 04:52:00 +0200

    2022

--- static/js/session.js
+++ static/js/session.js
@@ -181,6 +181,7 @@ function animationLoop(){
         timer = timer - 33;
     }
 
+    snapLabel.innerHTML = window.innerHeight + " " + innerH;
   
     width = window.innerWidth;
     height = window.innerWidth*3/4;
