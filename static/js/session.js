802a6f8733b9a6394dc086a31206aa5e9538dfc4
patakk <paolo.ceric@gmail.com>
2022-04-30 04:57:17 +0200

    2022

--- static/js/session.js
+++ static/js/session.js
@@ -188,7 +188,19 @@ function animationLoop(){
         if(!canvasRect)
             canvasRect = canvas.getBoundingClientRect();
 
+        if(!displayDefault)
+            displayDefault = canvas.style.display;
     
+        if(window.innerHeight < innerH){
+            canvas.style.display = "none";
+            snapLabel.style.display = "none";
+            saveLabel.style.display = "none";
+        }
+        else{
+            canvas.style.display = displayDefault;
+            snapLabel.style.display = displayDefault;
+            saveLabel.style.display = displayDefault;
+        }
 
         snapLabel.style.top = canvasRect['bottom']*1. + innerH*0.02 + "px";
         var snapRect = snapLabel.getBoundingClientRect();
