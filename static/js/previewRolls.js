e028871a56f7d164a8a9363753562e80748d3633
patakk <paolo.ceric@gmail.com>
2022-04-30 21:43:34 +0200

    2022

--- static/js/previewRolls.js
+++ static/js/previewRolls.js
@@ -5,7 +5,7 @@ var userName = null;
 
 function handleRollClick(e){
     userName = window.location.search.split("&")[0].split("=")[1];
-    window.location.href = '/foto/preview?userName=' + userName + "&rollName=" + e.srcElement.id;
+    window.location.href = '/foto/preview?userName=' + userName + "&rollName=" + e.srcElement.attributes.name.value;
 }
 
 
